ff7e089c3e2e07776c8d11c497018ff70ba37fad
iamyelex <yelex4love@gmail.com>
2022-06-24 18:12:12 +0800

    views: views created for the app

--- I4G010548K7S/blog/views.py
+++ I4G010548K7S/blog/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
+
+class PostListView(ListView):
+    model= Post
+    Template_name = 'post_list.html'
+
+
+class PostCreateView(CreateView):
+    model= Post
+    fields= '__all__'
+    success_url  = reverse_lazy('blog:all')
+    Template_name = 'post_form.html'
+
+
+class PostDetailView(DetailView):
+    model= Post
+    Template_name = 'post_detail.html'
+
+
+class PostUpdateView(UpdateView):
+    model= Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+    Template_name ='base.html'
+
+
+class PostDeleteView(DeleteView):
+    model= Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+    Template_name = 'post_confirm_delete.html'
